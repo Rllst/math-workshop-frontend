ffb63629716cc458624ef705299fe448879378bb
serhii.horoshkevych <serhii.horoshkevych@inventorsoft.co>
2023-03-16 12:46:02 +0200

    fix

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import {errorMessage, successMessage} from "../ToastHelper";
 
 const instance = axios.create({
-    baseURL: "http://localhost:8000/api",
+    baseURL: "https://8741p.localtonet.com/api",
     headers: {
         "Content-Type": "application/json",
         "Bypass-Tunnel-Reminder":'dsa',
